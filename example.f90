b0ad94774d5fe0f542dc97a0b97a2a1989c9eed6
JixingGE <49381977+JixingGE@users.noreply.github.com>
2022-05-03 22:34:15 -0400

    Update example.f90

--- example.f90
+++ example.f90
@@ -1,4 +1,3 @@
-
 program test
     use hashtable_lib
     implicit none
@@ -8,14 +7,14 @@ program test
     call mytable%init(3, 7) !! (n-keys, n-values)
     
     x = [1.0, 2.0, 3.0, 4.0, 5.0]
-    call mytable%add('H2', x, 5) !! the final is the dimension of x
+    call mytable%add('H2', x) !! the final is the dimension of x
     
     a = [1.0, 1.0, 1.0, 1.0, 1.0]
-    call mytable%add('CO', a, 5)
-    call mytable%add('CO', a, 5)
+    call mytable%add('CO', a)
+    call mytable%add('CO', a)
     
     b = [2.0, 2.0, 2.0, 2.0, 2.0, 7.0, 7.0]
-    call mytable%add('H2O', b, 7)
+    call mytable%add('H2O', b)
     
     call mytable%get('H2', y)
     print*,y
@@ -28,14 +27,13 @@ program test
     call mytable%del('H2')
     call mytable%list()
     
-    call mytable%add('H2', x, 5)
+    call mytable%add('H2', x)
     call mytable%list()
     
     call mytable%get('H2', y)
     print*,y
     
     call mytable%get('xxxx', y)
-    call mytable%add('NH3', x, 5)
+    call mytable%add('NH3', x)
     
 end
-
